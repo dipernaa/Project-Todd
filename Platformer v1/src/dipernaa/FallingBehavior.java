679a21771e75d22574785a83f3bdc6d8a15b023b
AlexShumov <user@a43f5a570e47419>
2014-01-19 13:01:47 +0400

    Fixed loads of bugs

--- Platformer v1/src/dipernaa/FallingBehavior.java
+++ Platformer v1/src/dipernaa/FallingBehavior.java
@@ -2,11 +2,16 @@ package dipernaa;
 
 public class FallingBehavior extends VerticalMovingBehavior {
 
+	protected double fallingSpeed = 0;
+	
 	@Override
 	public void moveVertically(Panel panel, Player player) {
-		if(fallingFrame >= FALLING_SPEED) {
-			player.y++;
-			panel.ys++;
+		if(fallingFrame >= 1) {
+			player.y += fallingSpeed / 10;
+			if(fallingSpeed < TERMINAL_VELOCITY) {
+				fallingSpeed += 0.5;
+			}
+			panel.ys += fallingSpeed / 10;
 			fallingFrame = 0;
 		} 
 		else {
